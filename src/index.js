bcd6bfd1e066d65d70810a9bda40f22656f63614
nilia2020 <jorge.niglia@gmail.com>
2021-08-24 19:27:50 -0300

    hasta BadgeList

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 //Importo el componente Badge
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css'
-import BadgeNew from './pages/BadgeNew'
+//import BadgeNew from './pages/BadgeNew'
+import Badges from './pages/Badges'
 //const element = <h1>Hello, Platzi Badges!!!!</h1>
 //const element = React.createElement('a', {href: 'https://platzi.com'}, 'Ir a Platzi')
 const container = document.getElementById('app')
 // ReactDOM.render(__elemnento__, __dónde__)
 // agrego < y /> a la clase Badge para que lo tome como un elemento
-ReactDOM.render(<BadgeNew/>, container)
+ReactDOM.render(<Badges/>, container)
 
